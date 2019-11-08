e9cefe64962d13b2557712a9a902c0e72a162653
LaboratoriaD163 <inapadfoot@yahoo.com>
2019-11-08 14:35:18 -0300

    adding carousel with posts

--- script-landing.js
+++ script-landing.js
@@ -1,52 +1,69 @@
 const urlUsers = 'https://jsonplaceholder.typicode.com/users'
 const urlPosts = 'https://jsonplaceholder.typicode.com/posts'
+let testimonialsUsers;
 fetch(urlUsers)
     .then(resUsers => resUsers.json())
     .then(resUsers => {
-         resUsers.map((el, index) => { 
-              const id = el.id;
-               console.log(id)
-            fetch(urlPosts)
-            .then(response =>response.json())
-            .then(response=>{
-                console.log(response)
-id.map(id=>
-    response.filter(el => el.id === id))          
-            }) 
-           
-           
-           
+
+        let users = resUsers
+        console.log(users)
+        fetch(urlPosts)
+            .then(response => response.json())
+            .then(response => {
+                let posts = response;
+                testimonialsUsers = objectWithNameAndPosts(users, posts)
+                console.log(testimonialsUsers)
+               testimonialsUsers.map((el, index) => { 
+
             document.getElementById("testimonials-peoples").innerHTML +=
 
                 index === 0 ?
                 `<div class="carousel-item active">
-                                <div class="carousel-testimonials">
-                                    <img src="./images/amalia-image.jpeg" class="d-block w-100" alt="...">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, ab
-                                        necessitatibus exercitationem rem libero rerum eos magnam quibusdam molestias
-                                        inventore unde architecto magni repellat laudantium aliquam labore? Inventore,
-                                        optio
-                                        harum.
-                                    </p>
-                                    <p>${resUsers[0].name}</p>
-                                </div>
-                            </div>
-                            `
-                            :                           
-                            `
-                            <div class="carousel-item">
                             <div class="carousel-testimonials">
                                 <img src="./images/amalia-image.jpeg" class="d-block w-100" alt="...">
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, ab
-                                    necessitatibus exercitationem rem libero rerum eos magnam quibusdam molestias
-                                    inventore unde architecto magni repellat laudantium aliquam labore? Inventore,
-                                    optio
-                                    harum.
+                                <p>${testimonialsUsers[0].posts[0]}
                                 </p>
-                                <p>${el.name}</p>
+                                <p>${testimonialsUsers[0].name}</p>
                             </div>
                         </div>
+                        ` :
+                `
+                        <div class="carousel-item">
+                        <div class="carousel-testimonials">
+                            <img src="./images/amalia-image.jpeg" class="d-block w-100" alt="...">
+                            <p>${el.posts[0]}
+                            </p>
+                            <p>${el.name}</p>
+                        </div>
+                    </div>
 
-                            `
+                        `
+        })
+
+                })
+            })
+
+
+const objectWithNameAndPosts = (users, posts) => {
+    console.log(users)
+    console.log(posts)
+
+    let arrayWithOnlyPosts = posts.reduce((post, element) => {
+        let id = element.userId
+        console.log(id)
+        let newPosts = posts.filter(el => el.userId === id)
+            .map(el => el.title)
+        post[id] = newPosts
+        return post
+    }, {})
+
+    let objectWithNameAndPostFromUsers = users.map(el => {
+        let value = arrayWithOnlyPosts[el.id]
+        return Object.assign({
+            name: el.name,
+            posts: value
         })
     })
+    return (objectWithNameAndPostFromUsers)
+}
+
